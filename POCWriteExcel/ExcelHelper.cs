c330c53600f20176e2a72157787be46fc59298ad
Kunlapath Paengsa <kunlapath@buzzebees.com>
2021-11-02 19:33:32 +0700

    Add project files.

---
+++ POCWriteExcel/ExcelHelper.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using OfficeOpenXml;
+
+namespace POCWriteExcel
+{
+    public class ExcelHelper
+    {
+        public string PathFile { get; }
+        public ExcelOptions Options { get; }
+
+        public DateFormatProvider DefaultDateFormatProvider => new DateFormatProvider
+        {
+            Format = "yyyy-MM-dd",
+            CultureInfo = new CultureInfo("en-US")
+        };
+
+        public ExcelHelper(string pathFile) : this(pathFile, new ExcelOptions())
+        { }
+
+        public ExcelHelper(string pathFile, ExcelOptions options)
+        {
+            PathFile = pathFile ?? throw new ArgumentNullException(nameof(pathFile));
+
+            Options = options;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        public FileInfo WriteFile<T>(string sheetName, IEnumerable<T> data) where T : class
+        {
+            var fileInfo = new FileInfo(PathFile);
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var sheet = package.Workbook.Worksheets.Add(sheetName ?? "sheet1");
+                var dataTable = data.ToDataTable();
+
+                var headerColumnNames = Options.IsWriteHeader && Options.CustomColumnNames.Any()
+                                        ? Options.CustomColumnNames.ToArray()
+                                        : dataTable.Columns.Cast<DataColumn>().Select(s => s.ColumnName).ToArray();
+
+                //Passing header name into cell
+                PassingHeaders(sheet, headerColumnNames);
+
+                //Passing data into cell
+                PassingData(sheet, data.ToDataTable());
+                package.Save();
+            }
+            return fileInfo;
+        }
+
+        private void PassingData(ExcelWorksheet sheet, DataTable data)
+        {
+            int startRow = Options.IsWriteHeader ? 2 : 1;
+            for (int row = 0; row < data.Rows.Count; row++)
+            {
+                for (int col = 0; col < data.Columns.Count; col++)
+                {
+                    var column = data.Columns[col];
+                    var value = data.Rows[row][col];
+
+                    if (column.DataType == typeof(DateTime) || column.DataType == typeof(DateTime?))
+                    {
+                        value = ConvertToDateTime(value);
+                    }
+
+                    sheet.Cells[startRow, col + 1].Value = value;
+                }
+                startRow++;
+            }
+        }
+
+        private void PassingHeaders(ExcelWorksheet sheet, string[] columnNames)
+        {
+            if (!Options.IsWriteHeader)
+            {
+                return;
+            }
+
+            int col = 1;
+            foreach (var name in columnNames)
+            {
+                sheet.Cells[1, col].Value = name;
+                col++;
+            }
+        }
+
+        private object ConvertToDateTime(object value)
+        {
+            if (value == null)
+            {
+                return value;
+            }
+
+            if (Options.DateFormatProvider.IsEmpty)//If empty use default.
+            {
+                value = Convert.ToDateTime(value, DefaultDateFormatProvider.CultureInfo)
+                               .ToString(DefaultDateFormatProvider.Format);
+            }
+            else if (!Options.DateFormatProvider.IsEmpty)
+            {
+                value = Convert.ToDateTime(value, Options.DateFormatProvider.CultureInfo)
+                               .ToString(Options.DateFormatProvider.Format);
+            }
+            else if (!string.IsNullOrWhiteSpace(Options.DateFormatProvider.Format))
+            {
+                value = Convert.ToDateTime(value).ToString(Options.DateFormatProvider.Format);
+            }
+            else if (Options.DateFormatProvider.CultureInfo != null)
+            {
+                value = Convert.ToDateTime(value, Options.DateFormatProvider.CultureInfo);
+            }
+
+            return value;
+        }
+
+        public class ExcelOptions
+        {
+            /// <summary>
+            /// Write header status. If true is write header, false no write.
+            /// </summary>
+            public bool IsWriteHeader { get; set; }
+            /// <summary>
+            /// Column name for write into excel file for custom.
+            /// But must define <see cref="IsWriteHeader"/> is true.
+            /// </summary>
+            public List<string> CustomColumnNames { get; set; }
+
+            /// <summary>
+            /// Provider for set format datetime data for write into excel file.
+            /// </summary>
+            public DateFormatProvider DateFormatProvider { get; set; }
+
+            public ExcelOptions()
+            {
+                CustomColumnNames = new List<string>();
+                DateFormatProvider = new DateFormatProvider();
+            }
+        }
+
+        public class DateFormatProvider
+        {
+            public string Format { get; set; }
+            public CultureInfo CultureInfo { get; set; }
+
+            internal bool IsEmpty => string.IsNullOrWhiteSpace(Format) && CultureInfo == null;
+        }
+    }
+
+    public static class HelperExtension
+    {
+        public static DataTable ToDataTable<T>(this IEnumerable<T> data, string tableName = "") where T : class
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var properties = typeof(T).GetProperties();
+            var results = new DataTable(tableName ?? typeof(T).Name);
+
+            //Create columns.
+            results.Columns.AddRange(properties.Select(s => new DataColumn(s.Name, s.PropertyType)).ToArray());
+
+            foreach (var row in data)
+            {
+                var newRow = results.NewRow();
+                foreach (var property in properties)
+                {
+                    newRow[property.Name] = property.GetValue(row);
+                }
+                results.Rows.Add(newRow);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ POCWriteExcel/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using OfficeOpenXml;
+
+namespace POCWriteExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Lenovo\Desktop\MyWorkbook.xlsx";
+            var fileInfo = new FileInfo(path);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+            var memberships = GetMemberships();
+            var excelHelper = new ExcelHelper(path, new ExcelHelper.ExcelOptions
+            {
+                IsWriteHeader = true
+                , CustomColumnNames = new List<string> { ""1, "", "" } 
+                ,
+                DateFormatProvider = new ExcelHelper.DateFormatProvider
+                {
+                    Format = "yyyy-MM-dd HH:mm:ss.ffff",
+                    CultureInfo = null//new CultureInfo("en-US")
+                }
+            });
+            excelHelper.WriteFile(nameof(Membership), memberships);
+        }
+
+        static void WriteExcelFile(FileInfo fileInfo)
+        {
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var sheet = package.Workbook.Worksheets.Add("Sheet1");
+                PassingMemberData(sheet);
+                package.Save();
+            }
+        }
+
+        static void PassingMemberData(ExcelWorksheet sheet)
+        {
+            int limit = 10;
+            //int limit = 1000000;
+            for (int i = 0; i < limit; i++)
+            {
+                var birthday = DateTime.Now.AddDays(-10);
+                var row = i + 1;
+                sheet.Cells[row, 1].Value = row;
+                sheet.Cells[row, 2].Value = Guid.NewGuid();
+                sheet.Cells[row, 3].Value = $"Member_{i}";
+
+                sheet.Cells[row, 4].Value = birthday.ToString("yyyy-MM-dd HH:mm:ss.ffff", new CultureInfo("en-US"));
+
+                sheet.Cells[row, 5].Value = DateTime.Now.Year - birthday.Year;
+                sheet.Cells[row, 6].Value = i % 2 == 0;
+                sheet.Cells[row, 7].Value = i * 10000m;
+                sheet.Cells[row, 8].Value = DateTime.Now;
+
+                //sheet.Cells["A1:Z1"].Value = "1";
+
+                //if (row == 1)
+                //{
+                //    for (int col = 9; col <= 500; col++)
+                //    {
+                //        sheet.Cells[row, col].Value = DateTime.Now;
+                //    }
+                //}
+
+                Console.WriteLine($"Write excel file row : {row}.");
+            }
+        }
+
+        static List<Membership> GetMemberships()
+        {
+            var memberships = new List<Membership>();
+            int limit = 100;
+            for (int i = 0; i < limit; i++)
+            {
+                var birthday = DateTime.Now.AddYears(-5);
+                memberships.Add(new Membership
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Member_{i}",
+                    Age = DateTime.Now.Year - birthday.Year,
+                    Birthday = birthday,
+                    IsActive = i % 3 == 0,
+                    Amount = i * 10000,
+                    CreatedDate = DateTime.Now
+                });
+            }
+            return memberships;
+        }
+    }
+
+    public class Membership
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool IsActive { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
